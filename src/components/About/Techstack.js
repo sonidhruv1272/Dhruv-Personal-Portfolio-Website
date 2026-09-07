c11f1ead937333045b8679a0690a944ee8a2c342
Dhruv Soni <sonidhruv1272@gmail.com>
2026-09-06 23:04:46 -0400

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+
 import { CgCPlusPlus } from "react-icons/cg";
+
 import {
   DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
   DiPython,
-  DiGit,
   DiJava,
+  DiGit,
 } from "react-icons/di";
-import {
-  SiFirebase,
-  SiPostgresql,
-} from "react-icons/si";
 
 function Techstack() {
   return (
@@ -21,32 +16,41 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <span style={{ fontSize: "0.55em" }}>MATLAB</span>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <span style={{ fontSize: "0.45em" }}>Arduino</span>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <span style={{ fontSize: "0.45em" }}>ESP32</span>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <span style={{ fontSize: "0.42em" }}>PCB Design</span>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <span style={{ fontSize: "0.42em" }}>Embedded</span>
       </Col>
     </Row>
   );
