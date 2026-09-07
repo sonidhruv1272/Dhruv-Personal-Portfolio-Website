6dac635d9169a8cbfa05be0585defbcbc404c6aa
Dhruv Soni <sonidhruv1272@gmail.com>
2026-09-06 23:04:01 -0400

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+
 import Particle from "../Particle";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/image.png";
 
+import laptopImg from "../../Assets/image.png";
 
 function About() {
   return (
     <Container fluid className="about-section">
       <Particle />
+
       <Container>
         <Row style={{ justifyContent: "center", padding: "10px" }}>
           <Col
@@ -21,10 +23,12 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Get to Know  <strong className="purple">ME</strong>
+              Get to Know <strong className="purple">ME</strong>
             </h1>
+
             <Aboutcard />
           </Col>
+
           <Col
             md={5}
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
@@ -33,15 +37,12 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
+
         <h1 className="project-heading">
           Professional <strong className="purple">Skillset </strong>
         </h1>
 
         <Techstack />
-
-        
-
-        
       </Container>
     </Container>
   );
