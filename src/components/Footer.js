2705a0dccb444cd6e04d0c8cc00f287382693010
Dhruv Soni <sonidhruv1272@gmail.com>
2026-09-06 23:07:17 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,47 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Harsh Shukla</h3>
+          <h3>Designed and Developed by Dhruv Soni</h3>
         </Col>
+
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} HS</h3>
+          <h3>Copyright © {year} DS</h3>
         </Col>
+
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/Harsh-Is-Coding"
+                href="https://github.com/sonidhruv1272"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
               </a>
             </li>
+
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/harsh-shukla-4b573523b/"
+                href="https://www.linkedin.com/in/dhruv-soni-6a7a12334/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
               </a>
             </li>
-            
           </ul>
         </Col>
       </Row>
